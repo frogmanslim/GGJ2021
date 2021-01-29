c026c2a0e21877b62084a77c67d8337376af18ee
Judah Rosaria <judahboy@live.com>
2021-01-29 20:26:11 +0100

    player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)){
+            MoveUp();
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            MoveDown();
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
+            MoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            MoveRight();
+        }
+    }
+
+    void MoveUp()
+    {
+        transform.localPosition += new Vector3(0,1,0);
+    }
+    void MoveDown()
+    {
+        transform.localPosition += new Vector3(0,-1,0);
+    }
+    void MoveLeft()
+    {
+        transform.localPosition += new Vector3(-1,0,0);
+    }
+    void MoveRight()
+    {
+        transform.localPosition += new Vector3(1,0,0);
+    }
+
+}
